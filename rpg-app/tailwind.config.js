672f2fc8a487e329a109fe7126c467149cd1cec5
rumbo <rumbo.9370@gmail.com>
2025-03-12 15:01:40 +0100

    added theams

--- rpg-app/tailwind.config.js
+++ rpg-app/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1C262D",
+        background: "#12191D",
+        secondary: "#26363E",
+        text: "#F4F4F4",
+        button: "#0072E9",
+        muted: "#7C98A9"
+      }
+    },
   },
   plugins: [],
 };
